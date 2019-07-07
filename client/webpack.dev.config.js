3fb0df76f000eb8a8d748e8acac23fa1b5ddf67b
Maja Miarecki <mmiarecka@gmail.com>
2019-07-07 11:23:20 +0200

    set up client dev environment

---
+++ client/webpack.dev.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  devtool: '#inline-source-map',
+  entry: [ path.resolve(__dirname, 'src/index.js') ],
+  output: {
+    path: path.resolve(__dirname, '../dist/client'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  devServer: {
+    publicPath: '/',
+    contentBase: path.resolve(__dirname, '../dist/client'),
+    watchContentBase: true,
+    compress: true
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './client/static/index.html',
+      filename: 'index.html'
+    })
+  ]
+};
