479487cc123a1defc37525e72910791d6248bd32
Maja Miarecki <mmiarecka@gmail.com>
2019-07-07 13:46:52 +0200

    set up server dev environment

---
+++ server/src/server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import path from 'path';
+
+const app = express();
+app.use(express.static(path.join(__dirname, '../../dist/client')));
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html', {
+    root: path.join(__dirname, '../../dist/client')
+  }, (err) => {
+    if (err) {
+      next(err)
+    } else {
+      console.log('Sent:', 'index.html');
+    }
+  })
+});
+
+// app.use(err => {
+//   console.log(err);
+// });
+
+app.listen('8081', () => {
+  console.log(`server runs on port 8081`);
+});
